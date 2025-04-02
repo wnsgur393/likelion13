2558b8ae45df9665c12c23c7c188de04caa599cd
wnsgur393 <wnsgur39393@gmail.com>
2025-04-02 21:48:05 +0900

    test

--- week2/src/main.jsx
+++ week2/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Intro from './intro.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Intro />
   </StrictMode>,
 )
